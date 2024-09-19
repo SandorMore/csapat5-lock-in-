8215ddba9d31501fb82bfe4cbedde31de1cdba6c
Your Name <you@example.com>
2024-09-19 09:30:21 +0200

    ez kinda jo

--- freakbob.py
+++ freakbob.py
@@ -1,49 +1,41 @@
-import abc
-from pickletools import int4
 import string
 import random
 
-# Számok bekérése és leellenőrzése
-isSzamJo = False
-while isSzamJo == False:
-    szam1 = int(input("Adja meg a számok asó határát: "))
+
+szamlista = []
+
+print("1. Adott darabszámú véletlen egész számok generálása adott határok között. A határokat és a darabszámot olvassa be a program!")
+print("2. Adott darabszámú véletlen szöveg generálása az angol ABC nagybetűiből vagy kisbetűiből. A szövegek hossza véletlen legyen 1 és 20 karakter között, a darabszámot olvasd be!")
+print("3. Ugyanazokat a paramétereket olvassa be, mint az 1. feladat, de itt nem generálja a számokat, hanem beolvassa a ki.txt tartalmát, és leellenőrzi, hogy megfelel-e a feltételeknek.")
+print("4. Ugyanazokat a paramétereket olvassa be, mint a 2. feladat, de itt nem generálja a szöveget, hanem beolvassa a ki.txt tartalmát, és leellenőrzi, hogy megfelel-e a feltételeknek.")
+kerdes = input("Válassz: ")
+if kerdes == "1":
+    x = int(input("Hányszor generáljon számot: "))
+    szam1 = int(input("Adja meg a számok alsó határát: "))
     szam2 = int(input("Adja meg a számok felső határát: "))
-    if isinstance(szam1, int) and isinstance(szam2, int):
-        print("Az adat jól lett megadva!")
-        isSzamJo = True
-    else:
-        print("Az adat nem lett jól megadva.")
-
-# Random szamok generálása
-
-genRndNum = random.randint(szam1, szam2)
-print(genRndNum)
-
-# Kisbetűk és nagybetűk listába helyeése
-
-kisbetuk = list(string.ascii_lowercase)
-nagybetuk = list(string.ascii_uppercase)
-
-# Egyesített lista kis- és nagybetűkkel
-
-angol_abc = kisbetuk + nagybetuk
-print(angol_abc)
-
-# Kiválasztandó random betűk megcsinálása
-isSzamJo1 = False
-while isSzamJo1 == False:
-    abcszam = int(input("Adja meg a számok asó határát: "))
-    if isinstance(abcszam, int):
-        print("Az adat jól lett megadva!")
-        if abcszam > 0 and abcszam <= 20:
-            isSzamJo1 = True
-        else:
-            print("Az adat nem lett jól megadva.")
-    else:
-        print("Nem jók az adatok.")
-
-genRndLi = random.sample(angol_abc, abcszam)
-genRanStr = ''.join(genRndLi)
-# Kiírás
-
-print(genRanStr)
+    for i in range(x):
+            genRndNum = random.randint(szam1, szam2)
+            szamlista.append(genRndNum)
+    f = open("ki.txt", "w", encoding="utf-8")
+    f.write(";".join(map(str, szamlista)))
+    f.close()
+if kerdes == "2":
+    kisbetuk = list(string.ascii_lowercase)
+    nagybetuk = list(string.ascii_uppercase)
+    angol_abc = kisbetuk + nagybetuk
+    szolista = []
+    cook = int(input("Hányszor generáljon: "))
+    for i in range(cook):
+        abcszam = random.randint(1,20)
+        genRndLi = random.sample(angol_abc, abcszam)
+        genRanStr = ''.join(genRndLi)
+        szolista.append(genRanStr)
+    f = open("ki.txt", "w", encoding="utf-8")
+    f.write(";".join(map(str, szolista)))
+    f.close()
+if kerdes == "3":
+    x = int(input("Hányszor generáljon számot: "))
+    szam1 = int(input("Adja meg a számok alsó határát: "))
+    szam2 = int(input("Adja meg a számok felső határát: "))
+    f = open("ki.txt", "r", encoding="utf-8")
+    lines = f.readlines()
