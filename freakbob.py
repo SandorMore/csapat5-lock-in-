0cef6eac661f53f47a5e196b0dd086a9dcfdf00d
Your Name <you@example.com>
2024-09-19 08:24:47 +0200

    erm what the sigma?

--- freakbob.py
+++ freakbob.py
@@ -1,3 +1,4 @@
+import abc
 from pickletools import int4
 import string
 import random
@@ -18,7 +19,7 @@ while isSzamJo == False:
 genRndNum = random.randint(szam1, szam2)
 print(genRndNum)
 
-# Kisbetűk és nagybetűk listába
+# Kisbetűk és nagybetűk listába helyeése
 
 kisbetuk = list(string.ascii_lowercase)
 nagybetuk = list(string.ascii_uppercase)
@@ -28,3 +29,21 @@ nagybetuk = list(string.ascii_uppercase)
 angol_abc = kisbetuk + nagybetuk
 print(angol_abc)
 
+# Kiválasztandó random betűk megcsinálása
+isSzamJo1 = False
+while isSzamJo1 == False:
+    abcszam = int(input("Adja meg a számok asó határát: "))
+    if isinstance(abcszam, int):
+        print("Az adat jól lett megadva!")
+        if abcszam > 0 and abcszam <= 20:
+            isSzamJo1 = True
+        else:
+            print("Az adat nem lett jól megadva.")
+    else:
+        print("Nem jók az adatok.")
+
+genRndLi = random.sample(angol_abc, abcszam)
+genRanStr = ''.join(genRndLi)
+# Kiírás
+
+print(genRanStr)
